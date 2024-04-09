4d0a3096befb066296fe02236b11150fc64a610f
IonutStanciu22 <boacaionut@gmail.com>
2024-04-09 19:52:00 +0300

    homework10

---
+++ src/test/java/edu/scoalainformala/homework10/MainTest.java
@@ -0,0 +1,19 @@
+package edu.scoalainformala.homework10;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
+
